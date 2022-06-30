c817f7ada43385086128aa4d7391ca0203b28e48
fabeto10 <f0414475@gmail.com>
2022-06-30 01:49:28 +0000

    arreglando casi terminado

--- src/models.py
+++ src/models.py
@@ -8,26 +8,29 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship(User)
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, ForeignKey("user.id"))
+    user =Column(String(250), nullable=False)
+    favorite = relationship("planet", backref="favorite")
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, ForeignKey("user.id"))
+    user= Column(String(250), nullable=False)
+    favorite = relationship("character", backref="favorite")
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
